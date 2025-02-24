1f78207bf09bc194c500254f747c3dee4c96017f
Emil Elton Nilsen <emil.nilsen9888@gmail.com>
2025-02-24 20:45:52 +0100

    Init struktur på alle topper

---
+++ src/content.config.ts
@@ -0,0 +1,19 @@
+import { defineCollection, z } from "astro:content";
+
+const ToppStatus = ["IKKE_GJENNOMFØRT", "GJENNOMFØRT", "UTILGJENGELIG"] as const
+
+const topperCollection = defineCollection({
+    schema: z.object({
+        status: z.enum(ToppStatus),
+        rang: z.number(),
+        land: z.string(),
+        høyestePunkt: z.string(),
+        moh: z.number(),
+        fom: z.string(),
+        tom: z.string()
+    })
+})
+
+export const collections = {
+    topper: topperCollection
+}
---
+++ src/content/topper/52_vatikanstaten.md
@@ -0,0 +1,11 @@
+---
+status: GJENNOMFØRT
+rang: 52
+land: Vatikanstaten
+høyestePunkt: Vatican hill
+moh: 75
+fom: '21.02.2025'
+tom: '23.02.2025'
+---
+
+# Vatikanstaten

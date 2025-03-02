5f7d5de856ca975e5ce76eba5a40992a075b5220
Emil Elton Nilsen <emil.nilsen9888@gmail.com>
2025-03-02 12:26:19 +0100

    Fjern base

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,5 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://perkynades.github.io',
-    base: 'eltons-europeiske-topper'
+    site: 'https://eltons-europeiske-topper.eu',
 });

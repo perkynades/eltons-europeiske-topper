ef2fa9dec8e97bfe65cc639022c03527d3ce66c2
Emil Elton Nilsen <emil.elton.nilsen@nav.no>
2025-07-10 12:41:19 +0200

    Legg til cache

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,4 +4,5 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
     site: 'https://eltons-europeiske-topper.eu',
+    cacheDir: './cache'
 });
